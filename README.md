a63a1bc8337f101d8bfded4f96bf82724d1d2dbd
randjelovic-jelena <125824089+randjelovic-jelena@users.noreply.github.com>
2023-06-13 08:49:27 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,11 @@
+# Newsletter-sign-up-form-with-success-message
+
+![newsletter-sign-up-with-success-message-main_index html_user_email=ghc%40hdcgh](https://github.com/randjelovic-jelena/Newsletter-sign-up-form-with-success-message/assets/125824089/627a2c51-e3f8-4b3d-8940-6b2583fd2a62)
+
+![message-newsletter-sign-up-with-success-message-main_index html_user_email=ghc%40hdcgh](https://github.com/randjelovic-jelena/Newsletter-sign-up-form-with-success-message/assets/125824089/62d692c6-c488-4500-b0b6-843677b58fff)
+
+![newsletter-sign-up-with-success-message-main_index html_user_email=ghc%40hdcgh(Surface Duo)](https://github.com/randjelovic-jelena/Newsletter-sign-up-form-with-success-message/assets/125824089/8f952f74-049e-495a-95d0-83fe19d06e1f)
+
+![newsletter-sign-up-with-success-message-main_index html_user_email=ghc%40hdcgh(Galaxy Note 3)](https://github.com/randjelovic-jelena/Newsletter-sign-up-form-with-success-message/assets/125824089/508020a1-5245-4bbd-bb9c-9b136abf1fc2)
+
+![message-newsletter-sign-up-with-success-message-main_index html_user_email=ghc%40hdcgh(iPhone SE)](https://github.com/randjelovic-jelena/Newsletter-sign-up-form-with-success-message/assets/125824089/c2a38e4d-31ee-4372-b853-e67f1f622e46)
---
+++ js/main.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const mainCard=document.querySelector('.main');
+const successCard=document.querySelector('.success-card');
+const form=document.getElementById('form');
+const userEmail=document.getElementById('email');
+const submitFormBtn=document.getElementById('send-email');
+const dismissBtn=document.getElementById('dismiss-btn');
+const userEmailSpan=document.querySelector('.user-entered-email');
+const errorMessageSpan=document.querySelector('.error-message');
+let validRegex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
+
+const reset=()=>{
+    userEmail.value='';
+}
+const showSuccess=()=>{
+    reset();
+    successCard.style.display='grid';
+    mainCard.style.display='none';
+}
+const closeSuccessCard=()=>{
+    successCard.style.display='none';
+    mainCard.style.display='grid';
+}
+const setErrorMessage=(message)=>{
+    userEmail.classList.add('error');
+    errorMessageSpan.innerText=message;
+
+    setTimeout(()=>{
+        userEmail.classList.remove('error');
+        errorMessageSpan.innerText='';
+        reset();
+    },3000);
+}
+const validateEmail=()=>{
+    let enteredEmail=userEmail.value;
+
+    if(enteredEmail===''){
+        setErrorMessage('Field is empty!')
+    }else if(enteredEmail.length<9){
+        setErrorMessage('Must contain at least 8 characters');
+    }else if(!enteredEmail.match(validRegex)){
+        setErrorMessage('Valid email required');
+		
+    }else{
+        return true;
+    }
+    reset();
+}
+
+dismissBtn.addEventListener('click',closeSuccessCard);
+submitFormBtn.addEventListener('click',(e)=>{
+    e.preventDefault();
+    if(validateEmail()){
+        showSuccess();
+    }
+})
